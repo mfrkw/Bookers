81371a4d614a59e6f623dc4b2774f56bb25534ec
mfrkw <xjdk3766@gmail.com>
2021-06-12 10:19:43 +0000

    [Add]ルート設定

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end 
+  
+  def eidt
+    @book = Book.find(params[:id])
+  end 
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end 
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destory
+    redirect_to books_path
+  end 
+  
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+
+end
