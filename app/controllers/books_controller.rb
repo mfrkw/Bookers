197e0f5733cde14f894499828d2b656c6e4328a7
mfrkw <xjdk3766@gmail.com>
2021-06-14 08:09:58 +0000

    [Add]サクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,23 +13,32 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+     flash[:success] ="Book was successfully created."
+     redirect_to book_path(book.id)
+    else 
+      render :index
+    end   
   end 
   
-  def eidt
+  def edit
     @book = Book.find(params[:id])
   end 
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+    flash[:success] = "Book was successfully updated."
     redirect_to book_path(book.id)
+    else
+      render "edit"
+    end 
   end 
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:success] = "Book was successfully destroyed."
     redirect_to books_path
   end 
   
