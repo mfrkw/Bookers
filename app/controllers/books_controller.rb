2ebafd582b53d621521f4df518e191033c48815c
mfrkw <xjdk3766@gmail.com>
2021-06-14 13:42:46 +0000

    [Add]エラーメッセージ途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,17 +2,19 @@ class BooksController < ApplicationController
 
   
   def index
-    @books = Book.all
+    @books =Book.all
     @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    
   end
 
   
   def create
-    book = Book.new(book_params)
+    book = Book.new(book_params) 
+    @books = Book.all
     if book.save
      flash[:success] ="Book was successfully created."
      redirect_to book_path(book.id)
